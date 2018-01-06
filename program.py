313faac22ea505a8e8a9a0086bf18b7666491e83
AVLPAMWZ <mstephens1234@gmail.com>
2018-01-06 23:10:51 +0000

    First version

---
+++ program.py
@@ -0,0 +1,7 @@
+print("----------------------------")
+print("      HELLO APP V2")
+print("----------------------------")
+
+user_name = input("What is your name? ")
+greeting = "Nice to meet you " + user_name + "!"
+print(user_name)
